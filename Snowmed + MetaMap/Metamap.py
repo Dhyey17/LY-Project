297db2745389ef434eaddc3204220c778a98055c
Dhyey Vyas <48405523+Dhyey17@users.noreply.github.com>
2023-03-18 02:25:34 +0530

    Updated metamap.py

--- Snowmed + MetaMap/Metamap.py
+++ Snowmed + MetaMap/Metamap.py
@@ -1,29 +1,28 @@
 from pymedtermino.snomedct import *
 from pymedtermino.umls import *
 from pymetamap import MetaMap
+
+note1 = ["Mrs. Jones came in today complaining of a lot of chest pain. She denies shortness of breath and fevers."]
 #
-# # note1 = "Mrs. Jones came in today complaining of a lot of chest pain. She denies shortness of breath and fevers."
-# #
-# # note2 = "Mr. Smith has been having pain in his knee for many weeks. It hurts when he walks. There is no swelling, erythema, or micromotion tenderness."
-# #
-# # note3 = "Sandy Lemon has been having headaches for two months that are associated with blurry vision and numbness in her right arm."
-# #
-# # # Put them all together in a list
-# # note_list = [note1, note2, note3]
-# #
+# note2 = "Mr. Smith has been having pain in his knee for many weeks. It hurts when he walks. There is no swelling, erythema, or micromotion tenderness."
 #
+# note3 = "Sandy Lemon has been having headaches for two months that are associated with blurry vision and numbness in her right arm."
+#
+# # Put them all together in a list
+# note_list = [note1, note2, note3]
+#
+
 # file = open("Sentences.txt")
 # data = file.read()
 # file.close()
-#
-# mm=MetaMap.get_instance("../Docs/public_mm/bin/metamap")
-#
-# concepts,error=mm.extract_concepts(data,composite_phrase=1)
-#
-# for concept in concepts:
-#     print(concept)
 
-cui="C0013404"
+mm = MetaMap.get_instance("/home/dhyey/Downloads/public_mm/bin/metamap")
 
+concepts, error = mm.extract_concepts(note1, composite_phrase=1)
 
-print(SNOMEDCT.search(""))
+for concept in concepts:
+    print(concept)
+
+# # cui = "C0013404"
+#
+# print(SNOMEDCT[302509004])
