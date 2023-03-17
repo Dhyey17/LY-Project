5c07df842e1e7940129099b333929a99ca92dc4a
Dhyey Vyas <dhyeyvyas1711@gmail.com>
2023-03-17 12:25:22 +0530

    metamap and snowmed working

--- Snowmed + MetaMap/Metamap.py
+++ Snowmed + MetaMap/Metamap.py
@@ -1,3 +1,29 @@
 from pymedtermino.snomedct import *
+from pymedtermino.umls import *
+from pymetamap import MetaMap
+#
+# # note1 = "Mrs. Jones came in today complaining of a lot of chest pain. She denies shortness of breath and fevers."
+# #
+# # note2 = "Mr. Smith has been having pain in his knee for many weeks. It hurts when he walks. There is no swelling, erythema, or micromotion tenderness."
+# #
+# # note3 = "Sandy Lemon has been having headaches for two months that are associated with blurry vision and numbness in her right arm."
+# #
+# # # Put them all together in a list
+# # note_list = [note1, note2, note3]
+# #
+#
+# file = open("Sentences.txt")
+# data = file.read()
+# file.close()
+#
+# mm=MetaMap.get_instance("../Docs/public_mm/bin/metamap")
+#
+# concepts,error=mm.extract_concepts(data,composite_phrase=1)
+#
+# for concept in concepts:
+#     print(concept)
 
-print(SNOMEDCT[302509004])
+cui="C0013404"
+
+
+print(SNOMEDCT.search(""))
